5a112419cb9628e38d9f1b7f0dfc2bb5a49b253e
PaPr <36095960+PaPaPR@users.noreply.github.com>
2021-11-12 21:13:11 +0800

    add Camera

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+#include "mv_video_capture.hpp"
+
+int main()
+{
+    mindvision::CameraParam CameraParams = mindvision::CameraParam(0,
+                                                                 mindvision::RESOLUTION_1280_X_800,
+                                                                 mindvision::EXPOSURE_5000);
+    mindvision::VideoCapture *mv_capture_ = new mindvision::VideoCapture(CameraParams);
+    
+    cv::Mat src_img_;
+
+    while (mv_capture_->isindustryimgInput())
+    {
+        src_img_ = mv_capture_->image();
+        if (!src_img_.empty())
+        cv::imshow("dafule", src_img_);
+        if (cv::waitKey(1) == 'q')
+            break;
+        mv_capture_->cameraReleasebuff();
+    }
+
+    return 0;
+}
