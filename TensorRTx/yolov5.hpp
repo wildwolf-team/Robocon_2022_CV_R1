0297347d9f8c6b609496e3d88749ce224bbeefaf
PaPr <36095960+PaPaPR@users.noreply.github.com>
2021-12-04 19:36:46 +0800

    feat: remove inference time calculate

--- TensorRTx/yolov5.hpp
+++ TensorRTx/yolov5.hpp
@@ -153,9 +153,9 @@ std::vector<Yolo::Detection> yolov5_v5_Rtx_start(cv::Mat img) {
     buffer_idx += size_image_dst;                                                                   // new<----
 
     // 推理 以及 输出推理时间
-    auto start = std::chrono::system_clock::now();
+    // auto start = std::chrono::system_clock::now();
     doInference(*context, stream, (void**)buffers, prob, BATCH_SIZE);
-    auto end = std::chrono::system_clock::now();
+    // auto end = std::chrono::system_clock::now();
     // std::cout << "inference time: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms" << std::endl;
 
     std::vector<std::vector<Yolo::Detection>> batch_res(1);
