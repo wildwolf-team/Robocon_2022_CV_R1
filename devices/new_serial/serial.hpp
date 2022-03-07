669067d404ce03618ed2ec8b7f2cc089e38fa614
PaPr <36095960+PaPaPR@users.noreply.github.com>
2022-03-07 17:42:46 +0800

    refactor: 串口发送与接收修改

--- devices/new_serial/serial.hpp
+++ devices/new_serial/serial.hpp
@@ -21,31 +21,23 @@ class RoboSerial : public serial::Serial {
   }
 
   void WriteInfo(RoboCmd &robo_cmd) {
-    struct temp_send_info_struct{
-      uint8_t start = (unsigned)'S';
-      float yaw_angle;
-      float pitch_angle;
-      float depth;
-      uint8_t detect_object;
-      uint8_t end = (unsigned)'E';
-    } __attribute__((packed));
-    temp_send_info_struct t1;
-    t1.yaw_angle = robo_cmd.yaw_angle.load();
-    t1.pitch_angle = robo_cmd.pitch_angle.load();
-    t1.depth = robo_cmd.depth.load();
-    t1.detect_object = robo_cmd.detect_object.load();
+    RoboCmdUartBuff robo_cmd_uart_temp;
+    robo_cmd_uart_temp.yaw_angle = robo_cmd.yaw_angle.load();
+    robo_cmd_uart_temp.pitch_angle = robo_cmd.pitch_angle.load();
+    robo_cmd_uart_temp.depth = robo_cmd.depth.load();
+    robo_cmd_uart_temp.detect_object = robo_cmd.detect_object.load();
 
-    this->write((uint8_t *)&t1, sizeof(t1));
+    this->write((uint8_t *)&robo_cmd_uart_temp, sizeof(robo_cmd_uart_temp));
   }
 
   void ReceiveInfo(RoboInf &robo_inf) {
-    RoboInf temp_info;
+    RoboInfUartBuff robo_inf_uart_temp;
     uint8_t temp;
     this->read(&temp, 1);
     while (temp != 'S')
       this->read(&temp, 1);
-    this->read((uint8_t *)&temp_info, sizeof(temp_info));
-    robo_inf.yaw_angle.store(temp_info.yaw_angle);
+    this->read((uint8_t *)&robo_inf_uart_temp, sizeof(robo_inf_uart_temp));
+    robo_inf.yaw_angle.store(robo_inf_uart_temp.yaw_angle);
   }
 
  private:
--- utils.hpp
+++ utils.hpp
@@ -2,15 +2,28 @@
 #include <atomic>
 
 struct RoboCmd {
-  std::atomic<float> pitch_angle = 0.f;
   std::atomic<float> yaw_angle = 0.f;
+  std::atomic<float> pitch_angle = 0.f;
   std::atomic<float> depth = 0.f;
   std::atomic<uint8_t> detect_object = false;
 };
 
+struct RoboCmdUartBuff{
+  uint8_t start = (unsigned)'S';
+  float yaw_angle;
+  float pitch_angle;
+  float depth;
+  uint8_t detect_object;
+  uint8_t end = (unsigned)'E';
+} __attribute__((packed));
+
 struct RoboInf {
   std::atomic<float> yaw_angle = 0;
 };
+
+struct RoboInfUartBuff {
+  float yaw_angle = 0;
+} __attribute__((packed));
 
 bool rectFilter(std::vector<Yolo::Detection> res, cv::Mat &img,
                 cv::Rect &rect) {
