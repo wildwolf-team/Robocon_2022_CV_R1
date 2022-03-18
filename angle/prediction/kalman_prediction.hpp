d2b697bf087f536d276e9395180b842c57eb14f3
PaPr <36095960+PaPaPR@users.noreply.github.com>
2022-03-18 13:11:10 +0800

    feat: kalman prediction config

--- angle/prediction/kalman_prediction.hpp
+++ angle/prediction/kalman_prediction.hpp
@@ -16,11 +16,11 @@ class KalmanPrediction : public Kalman<1, S> {
     _Kalman::Matrix_zxd H;
     H(0, 0) = 1;
     _Kalman::Matrix_xxd R;
-    R(0, 0) = 1;
+    R(0, 0) = 10;
     for (int i = 1; i < S; i++) {
       R(i, i) = 100;
     }
-    _Kalman::Matrix_zzd Q{4};
+    _Kalman::Matrix_zzd Q{20};
     _Kalman::Matrix_x1d init{0, 0};
     this->reset(A, H, R, Q, init, 0);
     start = std::chrono::system_clock::now();
@@ -29,7 +29,7 @@ class KalmanPrediction : public Kalman<1, S> {
   float Prediction(double ptz_yaw_angle, float depth) {
     auto end = std::chrono::system_clock::now();
     m_yaw = ptz_yaw_angle;
-    if (std::fabs(last_yaw - m_yaw) > (10 / 180. * M_PI)) {
+    if (std::fabs(last_yaw - m_yaw) > 5 ) {
       this->reset(m_yaw, std::chrono::duration_cast<std::chrono::milliseconds>(
                               end - start)
                               .count());
@@ -44,12 +44,13 @@ class KalmanPrediction : public Kalman<1, S> {
               .count());
     }
 
-    last_speed = state(1, 0);
-    double c_speed = state(1, 0) * depth;
-    double predict_time =
-        depth * 0.001 / (/*serial_.returnReceiveBulletVelocity()*/ 18);
-    double s_yaw = atan2(predict_time * c_speed, static_cast<int>(depth));
-    compensate_w = 8 * tan(s_yaw * 180 / CV_PI);
+    double c_speed = state(1, 0);
+    c_speed = (c_speed + last_speed) * 0.5;
+    last_speed = c_speed;
+    std::cout << "c_speed = " << c_speed << std::endl;
+    double predict_time = depth * 0.001 / 6 ;
+    double s_yaw        = atan2(predict_time * c_speed * depth * 0.001, 1);
+    compensate_w = 8 * tan(s_yaw);
     compensate_w *= 1000;
     compensate_w =
         (last_last_compensate_w + last_compensate_w + compensate_w) * 0.333;
